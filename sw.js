79ce17ea2711de3223217b938291510a6f9d2044
brilianeriko-95 <brilianeriko95@gmail.com>
2026-03-11 00:53:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
-const CACHE_NAME = 'turbin-log-v1';
+const CACHE_NAME = 'plant-monitor-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/manifest.json'
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) return response;
-        return fetch(event.request);
-      })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) return response;
+                return fetch(event.request);
+            })
+    );
 });
